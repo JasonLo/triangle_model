ab7fed251b4fcf00b230045fc88472806d84f414
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-04-01 21:44:35 +0000

    Updated attractor with noise

--- OSP/modeling.py
+++ OSP/modeling.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import activations, initializers, regularizers, Model
@@ -80,6 +79,10 @@ def input_s(e, t, f, i, gf, gi, kf, ki, tmax=3.8,
 
 
 class rnn(Layer):
+    """
+    Main time-averaged input implementation based on Plaut 96 (Fig 12.)
+    With additional attractor (cleanup) network                            
+    """
     # Use keras rnn layer seems more efficient, maybe upgrade later...
     def __init__(self, cfg, input_p_dignostic=False, name='rnn', **kwargs):
 
@@ -93,7 +96,7 @@ class rnn(Layer):
 
         self.w_oh = self.add_weight(
             name='w_oh',
-            shape=(self.cfg.o_input_dim, self.cfg.hidden_units),
+            shape=(self.cfg.input_dim, self.cfg.hidden_units),
             initializer=self.cfg.w_initializer,
             regularizer=self.weight_regularizer,
             trainable=True
@@ -101,7 +104,7 @@ class rnn(Layer):
 
         self.w_hp = self.add_weight(
             name='w_hp',
-            shape=(self.cfg.hidden_units, self.cfg.pho_units),
+            shape=(self.cfg.hidden_units, self.cfg.output_dim),
             initializer=self.cfg.w_initializer,
             regularizer=self.weight_regularizer,
             trainable=True
@@ -109,7 +112,7 @@ class rnn(Layer):
 
         self.w_pp = self.add_weight(
             name='w_pp',
-            shape=(self.cfg.pho_units, self.cfg.pho_units),
+            shape=(self.cfg.output_dim, self.cfg.output_dim),
             initializer=self.cfg.w_initializer,
             regularizer=self.weight_regularizer,
             trainable=True
@@ -117,7 +120,7 @@ class rnn(Layer):
 
         self.w_pc = self.add_weight(
             name='w_pc',
-            shape=(self.cfg.pho_units, self.cfg.cleanup_units),
+            shape=(self.cfg.output_dim, self.cfg.cleanup_units),
             initializer=self.cfg.w_initializer,
             regularizer=self.weight_regularizer,
             trainable=True
@@ -125,7 +128,7 @@ class rnn(Layer):
 
         self.w_cp = self.add_weight(
             name='w_cp',
-            shape=(self.cfg.cleanup_units, self.cfg.pho_units),
+            shape=(self.cfg.cleanup_units, self.cfg.output_dim),
             initializer=self.cfg.w_initializer,
             regularizer=self.weight_regularizer,
             trainable=True
@@ -140,7 +143,7 @@ class rnn(Layer):
         )
 
         self.bias_p = self.add_weight(
-            shape=(self.cfg.pho_units, ),
+            shape=(self.cfg.output_dim, ),
             name='bias_p',
             initializer='zeros',
             regularizer=self.weight_regularizer,
@@ -162,7 +165,7 @@ class rnn(Layer):
         # Spliting input_dim below (index = 2)
         if self.cfg.use_semantic == True:
             o_input, s_input = tf.split(
-                inputs, [self.cfg.o_input_dim, self.cfg.pho_units], 2
+                inputs, [self.cfg.input_dim, self.cfg.output_dim], 2
             )
         else:
             o_input = inputs
@@ -181,7 +184,7 @@ class rnn(Layer):
             tf.zeros((1, self.cfg.hidden_units), dtype=tf.float32)
         )
         self.input_p_list.append(
-            tf.zeros((1, self.cfg.pho_units), dtype=tf.float32)
+            tf.zeros((1, self.cfg.output_dim), dtype=tf.float32)
         )
         self.input_c_list.append(
             tf.zeros((1, self.cfg.cleanup_units), dtype=tf.float32)
@@ -232,7 +235,7 @@ class rnn(Layer):
             hp = tf.matmul(self.act_h_list[t - 1], w_hp)
             pp = tf.matmul(
                 self.act_p_list[t - 1],
-                tf.linalg.set_diag(w_pp, tf.zeros(self.cfg.pho_units))
+                tf.linalg.set_diag(w_pp, tf.zeros(self.cfg.output_dim))
             )  # Zero diagonal lock
             cp = tf.matmul(self.act_c_list[t - 1], w_cp)
 
@@ -268,7 +271,7 @@ class rnn(Layer):
 
     def compute_output_shape(self):
         return tensor_shape.as_shape(
-            [1, self.cfg.pho_units] + self.cfg.n_timesteps
+            [1, self.cfg.output_dim] + self.cfg.n_timesteps
         )
 
     def get_config(self):
@@ -278,6 +281,20 @@ class rnn(Layer):
 
 
 class attractor_rnn(Layer):
+    """
+    Recurrent Attractor Layer
+    
+    From HS04:
+    The phonological form of the target word was clamped on the phonological units for 2.66 units of time. 
+    Then a target signal was provided for the next 1.33 units of time, in which the network was required 
+    to retain the phonological pattern in the absence of external clamping.
+    
+    We have tau = 0.2 instead of 0.33 --> 2.66 units of time ~= 14 steps (2.8 units)
+    In the last 6 steps, we provide training signal for backprob
+    
+    Noise can also be added in each weight matrix by using cfg.w_xx_noise
+    Which will add a Gaussian noise (SD = noise level) at each time step
+    """
     def __init__(self, cfg, clamp_steps=14, **kwargs):
         super(attractor_rnn, self).__init__(**kwargs)
         self._name = 'rnn'
@@ -289,27 +306,27 @@ class attractor_rnn(Layer):
 
         self.w_pp = self.add_weight(
             name='w_pp',
-            shape=(self.cfg.pho_units, self.cfg.pho_units),
+            shape=(self.cfg.output_dim, self.cfg.output_dim),
             initializer=self.cfg.w_initializer,
             trainable=True
         )
 
         self.w_pc = self.add_weight(
             name='w_pc',
-            shape=(self.cfg.pho_units, self.cfg.cleanup_units),
+            shape=(self.cfg.output_dim, self.cfg.cleanup_units),
             initializer=self.cfg.w_initializer,
             trainable=True
         )
 
         self.w_cp = self.add_weight(
             name='w_cp',
-            shape=(self.cfg.cleanup_units, self.cfg.pho_units),
+            shape=(self.cfg.cleanup_units, self.cfg.output_dim),
             initializer=self.cfg.w_initializer,
             trainable=True
         )
 
         self.bias_p = self.add_weight(
-            shape=(self.cfg.pho_units, ),
+            shape=(self.cfg.output_dim, ),
             name='bias_p',
             initializer='zeros',
             trainable=True
@@ -344,13 +361,29 @@ class attractor_rnn(Layer):
         self.act_c_list.append(self.input_c_list[0] + 0.5)
 
         for t in range(1, self.cfg.n_timesteps + 1):
+            
+            if self.cfg.w_pp_noise != 0:
+                w_pp = self.inject_noise(self.w_pp, self.cfg.w_pp_noise)
+            else:
+                w_pp = self.w_pp
+
+            if self.cfg.w_pc_noise != 0:
+                w_pc = self.inject_noise(self.w_pc, self.cfg.w_pc_noise)
+            else:
+                w_pc = self.w_pc
+
+            if self.cfg.w_cp_noise != 0:
+                w_cp = self.inject_noise(self.w_cp, self.cfg.w_cp_noise)
+            else:
+                w_cp = self.w_cp
+            
 
             # ##### Phonology layer #####
             pp = tf.matmul(
                 self.act_p_list[t - 1],
-                tf.linalg.set_diag(self.w_pp, tf.zeros(self.cfg.pho_units))
+                tf.linalg.set_diag(w_pp, tf.zeros(self.cfg.output_dim))
             )  # Zero diagonal lock
-            cp = tf.matmul(self.act_c_list[t - 1], self.w_cp)
+            cp = tf.matmul(self.act_c_list[t - 1], w_cp)
 
             mem_p = self.input_p_list[t - 1]
             p = self.cfg.tau * (pp + cp +
@@ -366,7 +399,7 @@ class attractor_rnn(Layer):
             self.act_p_list.append(act_p)
 
             ##### Cleanup layer #####
-            pc = tf.matmul(self.act_p_list[t - 1], self.w_pc)
+            pc = tf.matmul(self.act_p_list[t - 1], w_pc)
 
             mem_c = self.input_c_list[t - 1]
             c = self.cfg.tau * (pc + self.bias_c) + (1 - self.cfg.tau) * mem_c
@@ -376,10 +409,14 @@ class attractor_rnn(Layer):
 
         return self.act_p_list[self.clamp_steps + 1:
                               ]  # Can get forgetting curve?
-
+    
+    def inject_noise(self, x, noise_sd):
+        noise = K.random_normal(shape=K.shape(x), mean=0., stddev=noise_sd)
+        return x + noise
+    
     def compute_output_shape(self):
         n = self.cfg.n_timesteps - self.clamp_steps
-        return tensor_shape.as_shape([1, self.cfg.pho_units] + n)
+        return tensor_shape.as_shape([1, self.cfg.output_dim] + n)
 
     def get_config(self):
         config = {'custom_cfg': self.cfg}
@@ -417,7 +454,7 @@ class attractor():
 
     def build_model(self):
         clamp_steps = 14
-        input_o = Input(shape=(self.cfg.pho_units, ))
+        input_o = Input(shape=(self.cfg.output_dim, ))
         rnn_model = attractor_rnn(self.cfg, clamp_steps)(input_o)
         self.model = Model(input_o, rnn_model)
 
@@ -453,3 +490,276 @@ def arm_attractor(model, attractor):
 
     model.set_weights(new_weights)
     return model
+
+
+class rnn_no_cleanup(Layer):
+    # In plaut version (rnn_v1), input are identical in O-->H path over timestep
+    # Use keras copy layer seems more efficient
+    def __init__(self, cfg, **kwargs):
+        super(rnn_no_cleanup, self).__init__(**kwargs)
+
+        self.cfg = cfg
+
+        self.rnn_activation = activations.get(cfg.rnn_activation)
+        self.weight_regularizer = regularizers.l2(cfg.regularizer_const)
+
+        self.w_oh = self.add_weight(
+            name='w_oh',
+            shape=(self.cfg.o_input_dim, self.cfg.hidden_units),
+            initializer=self.cfg.w_initializer,
+            regularizer=self.weight_regularizer,
+            trainable=True
+        )
+
+        self.w_hp = self.add_weight(
+            name='w_hp',
+            shape=(self.cfg.hidden_units, self.cfg.pho_units),
+            initializer=self.cfg.w_initializer,
+            regularizer=self.weight_regularizer,
+            trainable=True
+        )
+
+        self.w_pp = self.add_weight(
+            name='w_pp',
+            shape=(self.cfg.pho_units, self.cfg.pho_units),
+            initializer=self.cfg.w_initializer,
+            regularizer=self.weight_regularizer,
+            trainable=True
+        )
+
+        self.bias_h = self.add_weight(
+            shape=(self.cfg.hidden_units, ),
+            name='bias_h',
+            initializer='zeros',
+            trainable=True
+        )
+
+        self.bias_p = self.add_weight(
+            shape=(self.cfg.pho_units, ),
+            name='bias_p',
+            initializer='zeros',
+            trainable=True
+        )
+
+    def call(self, inputs):
+        # Hack for complying keras.layers.concatenate() format
+        # Dimension note: (batch, timestep, input_dim)
+        # Spliting input_dim below (index = 2)
+        if self.cfg.use_semantic == True:
+            o_input, s_input = tf.split(
+                inputs, [self.cfg.o_input_dim, self.cfg.pho_units], 2
+            )
+        else:
+            o_input = inputs
+
+        ### Trial level init ###
+        self.input_h_list = []
+        self.input_p_list = []
+
+        self.act_h_list = []
+        self.act_p_list = []
+
+        # Set input to 0
+        self.input_h_list.append(
+            tf.zeros((1, self.cfg.hidden_units), dtype=tf.float32)
+        )
+        self.input_p_list.append(
+            tf.zeros((1, self.cfg.pho_units), dtype=tf.float32)
+        )
+
+        # Set activations to 0.5
+        self.act_h_list.append(self.input_h_list[0] + 0.5)
+        self.act_p_list.append(self.input_p_list[0] + 0.5)
+
+        for t in range(1, self.cfg.n_timesteps + 1):
+            # print(f'Time step = {t}')
+
+            # Inject noise to weights in each time step
+            if self.cfg.w_oh_noise != 0:
+                w_oh = self.inject_noise(self.w_oh, self.cfg.w_oh_noise)
+            else:
+                w_oh = self.w_oh
+
+            if self.cfg.w_hp_noise != 0:
+                w_hp = self.inject_noise(self.w_hp, self.cfg.w_hp_noise)
+            else:
+                w_hp = self.w_hp
+
+            if self.cfg.w_pp_noise != 0:
+                w_pp = self.inject_noise(self.w_pp, self.cfg.w_pp_noise)
+            else:
+                w_pp = self.w_pp
+
+            ##### Hidden layer #####
+            oh = tf.matmul(o_input[:, t - 1, :], w_oh)
+            mem_h = self.input_h_list[t - 1]
+            h = self.cfg.tau * (oh + self.bias_h) + (1 - self.cfg.tau) * mem_h
+
+            self.input_h_list.append(h)
+            self.act_h_list.append(self.rnn_activation(h))
+
+            ##### Phonology layer #####
+            hp = tf.matmul(self.act_h_list[t - 1], w_hp)
+            pp = tf.matmul(
+                self.act_p_list[t - 1],
+                tf.linalg.set_diag(w_pp, tf.zeros(self.cfg.pho_units))
+            )  # Zero diagonal lock
+
+            mem_p = self.input_p_list[t - 1]
+
+            p = self.cfg.tau * (hp + pp +
+                                self.bias_p) + (1 - self.cfg.tau) * mem_p
+
+            if self.cfg.use_semantic == True:
+                p += s_input[:, t - 1, :]  # Inject semantic input
+
+            self.input_p_list.append(p)
+            self.act_p_list.append(self.rnn_activation(p))
+
+        return self.act_p_list[1:]
+
+    def inject_noise(self, x, noise_sd):
+        noise = K.random_normal(shape=K.shape(x), mean=0., stddev=noise_sd)
+        return x + noise
+
+    def compute_output_shape(self):
+        return tensor_shape.as_shape([1, cfg.pho_units] + cfg.n_timesteps)
+
+    def get_config(self):
+        config = {'custom_cfg': self.cfg, 'name': 'rnn'}
+        base_config = super(rnn_pho_task, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+class rnn_no_cleanup_no_pp(Layer):
+    # In plaut version (rnn_v1), input are identical in O-->H path over timestep
+    # Use keras copy layer seems more efficient
+    def __init__(self, cfg, **kwargs):
+        super(rnn_no_cleanup_no_pp, self).__init__(**kwargs)
+
+        self.cfg = cfg
+
+        self.rnn_activation = activations.get(cfg.rnn_activation)
+        self.weight_regularizer = regularizers.l2(cfg.regularizer_const)
+
+        self.w_oh = self.add_weight(
+            name='w_oh',
+            shape=(self.cfg.o_input_dim, self.cfg.hidden_units),
+            initializer=self.cfg.w_initializer,
+            regularizer=self.weight_regularizer,
+            trainable=True
+        )
+
+        self.w_hp = self.add_weight(
+            name='w_hp',
+            shape=(self.cfg.hidden_units, self.cfg.pho_units),
+            initializer=self.cfg.w_initializer,
+            regularizer=self.weight_regularizer,
+            trainable=True
+        )
+
+        self.w_pp = self.add_weight(
+            name='w_pp',
+            shape=(self.cfg.pho_units, self.cfg.pho_units),
+            initializer=self.cfg.w_initializer,
+            regularizer=self.weight_regularizer,
+            trainable=True
+        )
+
+        self.bias_h = self.add_weight(
+            shape=(self.cfg.hidden_units, ),
+            name='bias_h',
+            initializer='zeros',
+            trainable=True
+        )
+
+        self.bias_p = self.add_weight(
+            shape=(self.cfg.pho_units, ),
+            name='bias_p',
+            initializer='zeros',
+            trainable=True
+        )
+
+    def call(self, inputs):
+        # Hack for complying keras.layers.concatenate() format
+        # Dimension note: (batch, timestep, input_dim)
+        # Spliting input_dim below (index = 2)
+        if self.cfg.use_semantic == True:
+            o_input, s_input = tf.split(
+                inputs, [self.cfg.o_input_dim, self.cfg.pho_units], 2
+            )
+        else:
+            o_input = inputs
+
+        ### Trial level init ###
+        self.input_h_list = []
+        self.input_p_list = []
+
+        self.act_h_list = []
+        self.act_p_list = []
+
+        # Set input to 0
+        self.input_h_list.append(
+            tf.zeros((1, self.cfg.hidden_units), dtype=tf.float32)
+        )
+        self.input_p_list.append(
+            tf.zeros((1, self.cfg.pho_units), dtype=tf.float32)
+        )
+
+        # Set activations to 0.5
+        self.act_h_list.append(self.input_h_list[0] + 0.5)
+        self.act_p_list.append(self.input_p_list[0] + 0.5)
+
+        for t in range(1, self.cfg.n_timesteps + 1):
+            # print(f'Time step = {t}')
+
+            # Inject noise to weights in each time step
+            if self.cfg.w_oh_noise != 0:
+                w_oh = self.inject_noise(self.w_oh, self.cfg.w_oh_noise)
+            else:
+                w_oh = self.w_oh
+
+            if self.cfg.w_hp_noise != 0:
+                w_hp = self.inject_noise(self.w_hp, self.cfg.w_hp_noise)
+            else:
+                w_hp = self.w_hp
+
+            if self.cfg.w_pp_noise != 0:
+                w_pp = self.inject_noise(self.w_pp, self.cfg.w_pp_noise)
+            else:
+                w_pp = self.w_pp
+
+            ##### Hidden layer #####
+            oh = tf.matmul(o_input[:, t - 1, :], w_oh)
+            mem_h = self.input_h_list[t - 1]
+            h = self.cfg.tau * (oh + self.bias_h) + (1 - self.cfg.tau) * mem_h
+
+            self.input_h_list.append(h)
+            self.act_h_list.append(self.rnn_activation(h))
+
+            ##### Phonology layer #####
+            hp = tf.matmul(self.act_h_list[t - 1], w_hp)
+
+            mem_p = self.input_p_list[t - 1]
+
+            p = self.cfg.tau * (hp + self.bias_p) + (1 - self.cfg.tau) * mem_p
+
+            if self.cfg.use_semantic == True:
+                p += s_input[:, t - 1, :]  # Inject semantic input
+
+            self.input_p_list.append(p)
+            self.act_p_list.append(self.rnn_activation(p))
+
+        return self.act_p_list[1:]
+
+    def inject_noise(self, x, noise_sd):
+        noise = K.random_normal(shape=K.shape(x), mean=0., stddev=noise_sd)
+        return x + noise
+
+    def compute_output_shape(self):
+        return tensor_shape.as_shape([1, cfg.pho_units] + cfg.n_timesteps)
+
+    def get_config(self):
+        config = {'custom_cfg': self.cfg, 'name': 'rnn'}
+        base_config = super(rnn_pho_task, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
