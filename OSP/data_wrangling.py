7d7dad6862c3f57db08a670c6be8ac2e089d66c7
lcmjlo@gmail.com <l3gnonveppl#>
2020-11-23 20:06:59 +0000

    remove obsolete codes

--- OSP/data_wrangling.py
+++ OSP/data_wrangling.py
@@ -2,11 +2,12 @@ import numpy as np
 import pandas as pd
 import pickle
 
+
 def gen_pkey(p_file="/home/jupyter/tf/common/patterns/mappingv2.txt"):
     """ Read phonological patterns from the mapping file
     See Harm & Seidenberg PDF file
     """
-    
+
     mapping = pd.read_table(p_file, header=None, delim_whitespace=True)
     m_dict = mapping.set_index(0).T.to_dict('list')
     return m_dict
@@ -15,7 +16,7 @@ def gen_pkey(p_file="/home/jupyter/tf/common/patterns/mappingv2.txt"):
 def get_sampling_probability(df_train, implementation, stage=None):
     """ Return the sampling probability with different implementation
     Keyword arguments:
-    df_train -- training set in pandas dataframe format, contain WSJ word frequency (wf) and Zeno                    frequency (gr*) 
+    df_train -- training set in pandas dataframe format, contain WSJ word frequency (wf) and Zeno frequency (gr*) 
     implementation -- method of sampling
     stage (Chang implementation only) -- which stage in Chang sampling (default None)
 
@@ -41,8 +42,8 @@ def get_sampling_probability(df_train, implementation, stage=None):
     if implementation == "chang":
         if not (1 <= stage <= 14):
             raise ValueError("stage must be between 1-14")
-        cutoffs = [1000, 100, 50, 25, 10, 8, 6, 5, 4 ,3, 2, 1, 1, 0]
-        wf = df_train['gr' + str(stage)] 
+        cutoffs = [1000, 100, 50, 25, 10, 8, 6, 5, 4, 3, 2, 1, 1, 0]
+        wf = df_train['gr' + str(stage)]
         clip = wf.map(lambda x: x if (x > cutoffs[stage-1]) else 0)
 
         print(f"Removed words with <= {cutoffs[stage-1]} wpm.")
@@ -51,35 +52,6 @@ def get_sampling_probability(df_train, implementation, stage=None):
         compressed_wf = np.log(clip + 1)
 
     return np.array(compressed_wf/np.sum(compressed_wf), dtype="float32")
-
-
-class wf_manager():
-    """ Calculate sampling probability
-    Note: the probability must sum to 1 when passing it to np.random.choice()
-    """
-
-    def __init__(self, wf):
-        self.wf = np.array(wf)
-
-    def to_p(self, x):
-        return x / np.sum(x)
-
-    def samp_termf(self):
-        return self.to_p(self.wf)
-
-    def samp_log(self):
-        log = np.log(1 + self.wf)
-        return self.to_p(log)
-
-    def samp_hs04(self):
-        root = np.sqrt(self.wf) / np.sqrt(30000)
-        clip = root.clip(0.05, 1.0)
-        return self.to_p(clip)
-    
-    def samp_jay(self):
-        cap = self.wf.clip(0, 10000)
-        root = np.sqrt(cap)
-        return self.to_p(root)
 
 
 # Input for training set
@@ -101,7 +73,7 @@ def sample_generator(cfg, data):
 
         # Preallocate
         batch_s = np.zeros((cfg.batch_size, cfg.n_timesteps, cfg.output_dim))
-        
+
         # Time invarying output
         batch_y = [data.y_train[idx]] * cfg.output_ticks
 
@@ -165,8 +137,8 @@ def test_set_input(
                     ki=cfg.sem_param_ki,
                     hf=cfg.sem_param_hf,
                     hi=cfg.sem_param_hi,
-                    tmax=cfg.max_unit_time - cfg.tau # zero-indexing
-                )  
+                    tmax=cfg.max_unit_time - cfg.tau  # zero-indexing
+                )
                 batch_s[:, t, :] = np.tile(
                     np.expand_dims(s_cell, 1), [1, cfg.output_dim]
                 )
@@ -184,6 +156,7 @@ class my_data():
     This object load all clean data from disk (both training set and testing sets)
     Also calculate sampling_p according to cfg.sample_name setting
     """
+
     def __init__(self, cfg):
 
         self.sample_name = cfg.sample_name
@@ -199,21 +172,23 @@ class my_data():
         self.x_strain_wf = np.array(self.df_strain['wf'])
         self.x_strain_img = np.array(self.df_strain['img'])
         self.y_strain = np.load(input_path + 'y_strain.npz')['data']
-        
+
         self.df_grain = pd.read_csv(input_path + 'df_grain.csv', index_col=0)
         self.x_grain = np.load(input_path + 'x_grain.npz')['data']
         self.x_grain_wf = np.array(self.df_grain['wf'])
         self.x_grain_img = np.array(self.df_grain['img'])
         self.y_large_grain = np.load(input_path + 'y_large_grain.npz')['data']
         self.y_small_grain = np.load(input_path + 'y_small_grain.npz')['data']
-        
-        self.df_taraban = pd.read_csv(input_path + 'df_taraban.csv', index_col=0)
+
+        self.df_taraban = pd.read_csv(
+            input_path + 'df_taraban.csv', index_col=0)
         self.x_taraban = np.load(input_path + 'x_taraban.npz')['data']
         self.x_taraban_wf = np.array(self.df_taraban['wf'])
         self.x_taraban_img = np.array(self.df_taraban['img'])
         self.y_taraban = np.load(input_path + 'y_taraban.npz')['data']
-        
-        self.df_glushko = pd.read_csv(input_path + 'df_glushko.csv', index_col=0)
+
+        self.df_glushko = pd.read_csv(
+            input_path + 'df_glushko.csv', index_col=0)
         self.x_glushko = np.load(input_path + 'x_glushko.npz')['data']
         self.x_glushko_wf = np.array(self.df_glushko['wf'])
         self.x_glushko_img = np.array(self.df_glushko['img'])
@@ -222,9 +197,13 @@ class my_data():
         f.close()
         f = open(input_path + 'pho_glushko.pkl', "rb")
         self.pho_glushko = pickle.load(f)
-        f.close()  
+        f.close()
 
         self.phon_key = gen_pkey()
+
+        # Elevate for easier access
+        self.wf = np.array(self.df_train['wf'], dtype='float32')
+        self.img = np.array(self.df_train['img'], dtype='float32')
 
         print('==========Orthographic representation==========')
         print('x_train shape:', self.x_train.shape)
@@ -240,19 +219,5 @@ class my_data():
         print('y_large_grain shape:', self.y_large_grain.shape)
         print('y_small_grain shape:', self.y_small_grain.shape)
         print('y_taraban shape:', self.y_taraban.shape)
-        print('y_glushko shape: ({}, {})'.format(len(self.y_glushko.items()), len(self.y_glushko['beed'][0])))
-
-        self.gen_sample_p()
-        self.wf = np.array(self.df_train['wf'], dtype='float32')
-        self.img = np.array(self.df_train['img'], dtype='float32')
-
-    def gen_sample_p(self):
-        """Calculate sampling p by different methods"""
-        wf = wf_manager(self.df_train['wf'])
-        if self.sample_name == 'hs04':
-            self.sample_p = wf.samp_hs04()
-        if self.sample_name == 'jay':
-            self.sample_p = wf.samp_jay()
-        if self.sample_name == 'log':
-            self.sample_p = wf.samp_log()
-
+        print('y_glushko shape: ({}, {})'.format(
+            len(self.y_glushko.items()), len(self.y_glushko['beed'][0])))
