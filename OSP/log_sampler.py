2974787527d63e56b680faa9eeda4af68fee156b
Jason Lo <lcmjlo@gmail.com>
2020-11-25 20:24:40 +0000

    rerun sampler test

--- OSP/log_sampler.py
+++ OSP/log_sampler.py
@@ -1,3 +1,5 @@
+# %% Environment
+import importlib
 import altair as alt
 import meta
 import data_wrangling
@@ -5,41 +7,43 @@ import pandas as pd
 from IPython.display import clear_output
 
 alt.data_transformers.disable_max_rows()
+importlib.reload(data_wrangling)
+
 cfg = meta.model_cfg(json_file='models/booboo/model_config.json')
 data = data_wrangling.MyData()
 
 
 def dry_run_sampler(sample_name, cfg):
     cfg.sample_name = sample_name
-    sampler = data_wrangling.Sampling(cfg, data)
+    sampler = data_wrangling.Sampling(cfg, data, debugging=True)
     next(sampler.sample_generator())
     while sampler.debug_log_epoch[-1] <= 100:
-        print(sampler.debug_log_epoch)
+        print(f"Running {sample_name} epoch:{sampler.debug_log_epoch[-1]}")
         next(sampler.sample_generator())
         clear_output(wait=True)
 
     df_corpus_size = pd.DataFrame({"epoch": sampler.debug_log_epoch,
                                    "corpus_size": sampler.debug_log_corpus_size})
     df_corpus_size.to_csv(
-        f"working/dynamic_frequency/{sample_name}_corpus_size.csv")
+        f"dynamic_frequency/{sample_name}_corpus_size.csv")
     sampler.debug_log_dynamic_wf.to_csv(
-        f"working/dynamic_frequency/{sample_name}_dynamic_corpus.csv")
+        f"dynamic_frequency/{sample_name}_dynamic_corpus.csv")
     print("All done.")
 
 
-# Dry run and log dynamic corpus
+# %% Dry run and log dynamic corpus
 [dry_run_sampler(sample_name, cfg)
  for sample_name in ['experimental', 'chang', 'hs04', 'jay']]
 
 
-# Compile corpus size
-df1 = pd.read_csv('working/dynamic_frequency/chang_corpus_size.csv')
+# %% Visualize corpus size
+df1 = pd.read_csv('dynamic_frequency/chang_corpus_size.csv')
 df1['sample_name'] = 'chang'
-df2 = pd.read_csv('working/dynamic_frequency/experimental_corpus_size.csv')
+df2 = pd.read_csv('dynamic_frequency/experimental_corpus_size.csv')
 df2['sample_name'] = 'continuous'
-df3 = pd.read_csv('working/dynamic_frequency/hs04_corpus_size.csv')
+df3 = pd.read_csv('dynamic_frequency/hs04_corpus_size.csv')
 df3['sample_name'] = 'hs04'
-df4 = pd.read_csv('working/dynamic_frequency/jay_corpus_size.csv')
+df4 = pd.read_csv('dynamic_frequency/jay_corpus_size.csv')
 df4['sample_name'] = 'jay'
 df = pd.concat([df1, df2, df3, df4], axis=0)
 del df1, df2, df3, df4
@@ -53,17 +57,17 @@ plot_dcs = alt.Chart(df).mark_line().encode(
 
 plot_dcs
 
-plot_dcs.save('working/dynamic_frequency/corpus_size.html')
+plot_dcs.save('dynamic_frequency/corpus_size.html')
 
 
-# Compile dynamic corpus
-df1 = pd.read_csv('working/dynamic_frequency/chang_dynamic_corpus.csv')
+# %% Visualize dynamic corpus
+df1 = pd.read_csv('dynamic_frequency/chang_dynamic_corpus.csv')
 df1['sample_name'] = 'chang'
-df2 = pd.read_csv('working/dynamic_frequency/experimental_dynamic_corpus.csv')
+df2 = pd.read_csv('dynamic_frequency/experimental_dynamic_corpus.csv')
 df2['sample_name'] = 'continuous'
-df3 = pd.read_csv('working/dynamic_frequency/hs04_dynamic_corpus.csv')
+df3 = pd.read_csv('dynamic_frequency/hs04_dynamic_corpus.csv')
 df3['sample_name'] = 'hs04'
-df4 = pd.read_csv('working/dynamic_frequency/jay_dynamic_corpus.csv')
+df4 = pd.read_csv('dynamic_frequency/jay_dynamic_corpus.csv')
 df4['sample_name'] = 'jay'
 
 df = pd.concat([df1, df2, df3, df4], axis=0)
@@ -71,16 +75,14 @@ del df1, df2, df3, df4
 
 # Only select Strain items
 sel_df = df.loc[df.word.isin(data.df_strain.word)]
-
 sel_df = sel_df.melt(id_vars=['word', 'sample_name'],
-             var_name='epoch_label', value_name='wf')
+                     var_name='epoch_label', value_name='wf')
 sel_df['epoch'] = sel_df.epoch_label.apply(lambda x: int(x[12::]) - 1)
-
 sel_df.rename(columns={'wf': 'dynamic_wf'}, inplace=True)
 
 # Down-sample epoch
 sel_epoch = [0, 1, 3, 5, 7, 10, 30, 40, 50, 80, 100]
-sel_df = sel_df.loc[sel_df.epoch.isin(sel_epoch),]
+sel_df = sel_df.loc[sel_df.epoch.isin(sel_epoch), ]
 
 df_strain = pd.merge(sel_df, data.df_strain, "left", on='word')
 df_strain['cond'] = df_strain.frequency + '_' + df_strain.pho_consistency
@@ -95,12 +97,14 @@ plot_corpus_epoch = alt.Chart(df_strain).mark_line().encode(
     row="pho_consistency:N"
 ).interactive().properties(title="Dynamic frequency")
 
-plot_corpus_epoch.save('working/dynamic_frequency/strain_dynamic_frequency.html')
+plot_corpus_epoch.save(
+    'dynamic_frequency/strain_dynamic_frequency.html')
 
 
-# Relationship between actual corpus frequency and dynamic frequency
+# %% Relationship between actual corpus frequency and dynamic frequency
 input_dropdown = alt.binding_select(options=sel_epoch)
-selection_epoch = alt.selection_single(fields=['epoch'], bind=input_dropdown, init={'epoch': 100} )
+selection_epoch = alt.selection_single(
+    fields=['epoch'], bind=input_dropdown, init={'epoch': 100})
 selection_sampling = alt.selection_multi(fields=['sample_name'], bind='legend')
 
 plot_ff = alt.Chart(df_strain).mark_point().encode(
@@ -111,4 +115,4 @@ plot_ff = alt.Chart(df_strain).mark_point().encode(
     tooltip=["sample_name", "word", "static_wf", "dynamic_wf"]
 ).add_selection(selection_epoch, selection_sampling).transform_filter(selection_epoch).interactive()
 
-plot_ff.save('working/dynamic_frequency/strain_static_dynamic_wf.html')
+plot_ff.save('dynamic_frequency/strain_static_dynamic_wf.html')
