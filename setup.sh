0fb3a132b82427f6e5ae6878168c2d59f31621aa
Jason Lo <lcmjlo@gmail.com>
2021-02-06 04:54:48 +0000

    install nbstripout

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
-echo "/home/jupyter/tf/src" > /opt/conda/lib/python3.7/site-packages/tfsrc.pth
+echo "/home/jupyter/tf/src" > /opt/conda/lib/python3.7/site-packages/tf_src.pth
 pip install -r /home/jupyter/tf/requirements.txt
+mkdir -p ~/.config/git
+nbstripout --install --global --attributes=~/.config/git/attributes
