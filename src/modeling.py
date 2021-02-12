8b8d3760a63f5a4f5a7146158acda1e8aaf531fe
Jason Lo <lcmjlo@gmail.com>
2021-02-12 22:48:45 +0000

    remove print while set new active task

--- src/modeling.py
+++ src/modeling.py
@@ -240,7 +240,7 @@ class HS04Model(tf.keras.Model):
         self.built = True
 
     def set_active_task(self, task):
-        print(f"Activate task: {task}")
+        # print(f"Activate task: {task}")
         self.active_task = task
 
     def call(self, inputs, training=None):
