1a9df6b0a797b18d1741785f84f9fa912a6d8dff
Jason Lo <lcmjlo@gmail.com>
2021-02-23 21:00:36 +0000

    include git user config

--- setup.sh
+++ setup.sh
@@ -4,5 +4,7 @@ pip install -r /home/jupyter/tf/requirements.txt
 mkdir -p ~/.config/git
 nbstripout --install --global --attributes=~/.config/git/attributes
 cd /home/jupyter/tf
+git config --global user.name "Jason Lo"
+git config --global user.email "lcmjlo@gmail.com"
 git config --global credential.helper store
 git fetch
