14f0e27b7de5c5fc452b238e1247ba7c41a734b2
Jason Lo <lcmjlo@gmail.com>
2021-04-06 19:51:33 +0000

    added sampling p correlation

--- issues/semantic_diagnostic/semantic_feature_vs_img.py
+++ issues/semantic_diagnostic/semantic_feature_vs_img.py
@@ -1,6 +1,7 @@
 #%% 
 import data_wrangling
 import pandas as pd
+import numpy as np
 import altair as alt
 # %% Load test sets from file
 d = data_wrangling.MyData()
@@ -14,6 +15,7 @@ def get_n_features(word):
         return(sum(semantic_vector)) 
 
 
+
 # %% Parse Cortese
 cortese = pd.read_csv(
     "../preprocessing/cortese2004norms.csv", skiprows=9, na_filter=False
@@ -22,11 +24,23 @@ cortese = cortese[["item", "rating"]]
 cortese = cortese.rename({"item": "word", "rating":"img"}, axis=1)
 cortese["n_features"] = cortese.word.apply(get_n_features)
 
+cortese = cortese.merge(d.df_train[['word', 'wf']])
 
-# %% Plot
-p = alt.Chart(cortese).mark_point().encode(x="img", y="n_features", tooltip=["word", "img", "n_features"])
-p + p.transform_regression('img', 'n_features').mark_line(color="red")
+# %% Also get probability by different implementation
+cortese["p_hs04"] = data_wrangling.Sampling.get_sampling_probability(cortese, "hs04")
+cortese["p_zevin"] = data_wrangling.Sampling.get_sampling_probability(cortese, "jay")
+cortese["log_wf"] = data_wrangling.Sampling.get_sampling_probability(cortese, "log")
 
+
+# %% Plot IMG vs. n_features
+nf = alt.Chart(cortese).mark_point().encode(x="img", y="n_features", tooltip=["word", "img"])
+nf += nf.transform_regression('img', 'n_features').mark_line(color="red")
+nf.properties(title = cortese.corr())
 # %% Pearson's r
 cortese.corr()
 
+# %%
+p = alt.Chart(cortese).mark_point().encode(x="img", y="p_hs04", tooltip=["word", "p_hs04"])
+p + p.transform_regression('img', 'p_hs04').mark_line(color="red")
+
+# %%
