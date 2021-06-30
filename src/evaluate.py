8ba60403ceb035849b46006d815b0fd2259b1c5b
Jason Lo <lcmjlo@gmail.com>
2021-06-30 16:17:12 +0000

    working on new evaluator

--- src/evaluate.py
+++ src/evaluate.py
@@ -11,6 +11,7 @@ import altair as alt
 
 alt.data_transformers.disable_max_rows()
 
+
 def gen_pkey(p_file="/home/jupyter/tf/dataset/mappingv2.txt"):
     """Read phonological patterns from the mapping file
     See Harm & Seidenberg PDF file
@@ -20,7 +21,9 @@ def gen_pkey(p_file="/home/jupyter/tf/dataset/mappingv2.txt"):
     m_dict = mapping.set_index(0).T.to_dict("list")
     return m_dict
 
+
 phon_key = gen_pkey()
+
 
 def get_pronunciation_fast(act, phon_key):
     phonemes = list(phon_key.keys())
@@ -35,22 +38,22 @@ def get_pronunciation_fast(act, phon_key):
         out += phonemes[x]
     return out
 
+
 def get_batch_pronunciations_fast(act, phon_key):
     return np.apply_along_axis(get_pronunciation_fast, 1, act, phon_key)
-
 
 
 class TestSet:
     """Universal test set object for evaluating model results
     1. Single condition, single metric, single value output for maximum capatibility
-    2. Model level info should be stored at separate table, and merge it at the end
+    2. Model level info should be stored at separate table, and merge it in the end
     """
 
     pho_acc = metrics.PhoAccuracy("acc")
     right_side_acc = metrics.RightSideAccuracy("acc")
     sse = metrics.SumSquaredError("sse")
-    act1 = metrics.OutputOfOneTarget("act1")
-    act0 = metrics.OutputOfZeroTarget("act0")
+    # act1 = metrics.OutputOfOneTarget("act1")
+    # act0 = metrics.OutputOfZeroTarget("act0")
 
     def __init__(
         self,
@@ -76,7 +79,7 @@ class TestSet:
         self.result = None
 
     def _convert_dict_to_df(self, x):
-        
+
         # Flatten the nested output dictionary
         self._flat_dict = {
             (epoch, y, timetick, item, metric): {
@@ -88,22 +91,25 @@ class TestSet:
             for metric in x[epoch][y][timetick].keys()
             for item in x[epoch][y][timetick][metric].keys()
         }
-        
+
         # Create pd df and pivot by metric as column
-        
+
         df = pd.DataFrame.from_dict(self._flat_dict, orient="index")
         df.index.rename(["epoch", "y", "timetick", "item", "metric"], inplace=True)
-        df = df.reset_index().pivot(
+        df = (
+            df.reset_index()
+            .pivot(
                 index=["epoch", "timetick", "y", "item"],
                 columns="metric",
                 values="value",
-             ).reset_index()
+            )
+            .reset_index()
+        )
 
         # automatically convert to best dtype
         return df.convert_dtypes()
 
-    
-    def eval_all(self, label_dict=None):       
+    def eval_all(self, label_dict=None):
         df = pd.DataFrame()
         for epoch in tqdm(self.cfg.saved_epoches, desc=f"Evaluating {self.name}"):
             output = {}
@@ -131,16 +137,20 @@ class TestSet:
         pred_y = self.model([self.x_test] * self.cfg.n_timesteps)
 
         output = {}
-        if (self.task in ("triangle")):
+        if self.task in ("triangle"):
             output["pho"] = self._eval_one_y(pred_y[0], self.y_test[0], y_name="pho")
             output["sem"] = self._eval_one_y(pred_y[1], self.y_test[1], y_name="sem")
-        elif (self.task in ("pho_sem", "sem_sem", "ort_sem")):
+        elif self.task in ("pho_sem", "sem_sem", "ort_sem"):
             output["sem"] = self._eval_one_y(pred_y, self.y_test, y_name="sem")
-        elif (self.task in ("sem_pho", "pho_pho", "ort_pho")):
+        elif self.task in ("sem_pho", "pho_pho", "ort_pho"):
             output["pho"] = self._eval_one_y(pred_y, self.y_test, y_name="pho")
-        elif (self.task in ("exp_osp")):
-            output["pho"] = self._eval_one_y(pred_y["act_p"], self.y_test[0], y_name="pho")
-            output["sem"] = self._eval_one_y(pred_y["act_s"], self.y_test[1], y_name="sem")
+        elif self.task in ("exp_osp"):
+            output["pho"] = self._eval_one_y(
+                pred_y["act_p"], self.y_test[0], y_name="pho"
+            )
+            output["sem"] = self._eval_one_y(
+                pred_y["act_s"], self.y_test[1], y_name="sem"
+            )
         else:
             print(f"{self.task} task does not exist in evaluator")
 
@@ -172,40 +182,37 @@ class TestSet:
         # Use different acc depending on output nature
         if y_name == "pho":
             acc = self.pho_acc.item_metric(true_y, pred_y)
-            output["acc"] = dict(
-                zip(self.testitems, acc)
-            )
+            output["acc"] = dict(zip(self.testitems, acc))
             output["pho_symbol"] = dict(
                 zip(self.testitems, get_batch_pronunciations_fast(pred_y, phon_key))
             )
         elif y_name == "sem":
             acc = self.right_side_acc.item_metric(true_y, pred_y)
-            output["acc"] = dict(
-                zip(self.testitems, acc)
-            )
+            output["acc"] = dict(zip(self.testitems, acc))
 
         # SSE
         sse = self.sse.item_metric(true_y, pred_y)
         output["sse"] = dict(zip(self.testitems, sse))
 
         conditional_sse = sse
-        conditional_sse[acc==0] = np.nan
+        conditional_sse[acc == 0] = np.nan
         output["conditional_sse"] = dict(zip(self.testitems, conditional_sse))
-        
+
         # Activations
-        this_act0 = self.act0.item_metric(true_y, pred_y)
-        this_act1 = self.act1.item_metric(true_y, pred_y)
-        output["act0"] = dict(zip(self.testitems, this_act0))
-        output["act1"] = dict(zip(self.testitems, this_act1))
-        
+        # this_act0 = self.act0.item_metric(true_y, pred_y)
+        # this_act1 = self.act1.item_metric(true_y, pred_y)
+        # output["act0"] = dict(zip(self.testitems, this_act0))
+        # output["act1"] = dict(zip(self.testitems, this_act1))
+
         return output
+
 
 class EvalOral:
     """Bundle of testsets for Oral stage
     Not finished... only have train strain and taraban cortese img
     """
 
-    TESTSETS_NAME = ("train",  "strain",  "taraban")
+    TESTSETS_NAME = ("train", "strain", "taraban")
 
     def __init__(self, cfg, model, data):
         self.cfg = cfg
@@ -247,11 +254,9 @@ class EvalOral:
             results = self.run_eval[testset_name]()
         else:
             print("Evaluation results found, loaded from file.")
-                  
-
 
     def _eval_oral_tasks(self, testset_name):
-        """ The oral evalution consists of multiple tasks, sp, ps, pp, ss
+        """The oral evalution consists of multiple tasks, sp, ps, pp, ss
         This function will:
         1. Create the four tasks (TestSet object) based on testset_name
         2. Evaluate the tasks
@@ -264,7 +269,7 @@ class EvalOral:
 
         for this_task in tasks:
 
-            x, y = this_task.split('_')
+            x, y = this_task.split("_")
             this_testset_object = TestSet(
                 name=testset_name,
                 cfg=self.cfg,
@@ -315,7 +320,7 @@ class EvalOral:
 
         for testset_name in testsets:
             df = df.append(self._eval_oral_tasks(testset_name), ignore_index=True)
-            
+
         df.to_csv(
             os.path.join(self.cfg.path["model_folder"], "eval", "homophone_item_df.csv")
         )
@@ -327,27 +332,28 @@ class EvalOral:
         )
 
         mean_df.to_csv(
-            os.path.join(
-                self.cfg.path["model_folder"], "eval", "homophone_mean_df.csv"
-            )
+            os.path.join(self.cfg.path["model_folder"], "eval", "homophone_mean_df.csv")
         )
-        
+
         return df
-    
+
     def _eval_img(self):
         df = pd.DataFrame()
-        testsets = ("cortese_3gp_high_img", "cortese_3gp_med_img", "cortese_3gp_low_img")
-        
+        testsets = (
+            "cortese_3gp_high_img",
+            "cortese_3gp_med_img",
+            "cortese_3gp_low_img",
+        )
+
         for testset_name in testsets:
             df = df.append(self._eval_oral_tasks(testset_name), ignore_index=True)
-            
+
         df.to_csv(
             os.path.join(self.cfg.path["model_folder"], "eval", "img_item_df.csv")
         )
-        
+
         return df
-        
-    
+
     def _eval_strain(self):
         df = pd.DataFrame()
         testsets = (
@@ -426,30 +432,20 @@ class EvalOral:
 
         return df
 
+
 class EvalReading:
     """Bundle of testsets"""
 
-    TESTSETS_NAME = ("train", "strain", "grain", "taraban", "cortese")
+    TESTSETS_NAME = ("strain", "grain")
 
     def __init__(self, cfg, model, data):
         self.cfg = cfg
         self.model = model
         self.data = data
 
-        self.train_mean_df = None
         self.strain_mean_df = None
         self.grain_mean_df = None
-        self.taraban_mean_df = None
-        self.cortese_mean_df = None
-        
-        # Setup database if in batch_mode
-        if self.cfg.batch_name is not None:
-            self.batch_mode = True
-            sqlite_file = os.path.join(self.cfg.path["batch_folder"], "batch_results.sqlite")
-            self.con = sqlite3.connect(sqlite_file)
-        else:
-            self.batch_mode = False
-        
+
         # Load eval results from file
         for _testset_name in self.TESTSETS_NAME:
             try:
@@ -470,18 +466,13 @@ class EvalReading:
             "taraban": self._eval_taraban,
             "cortese": self._eval_cortese,
         }
-        
+
     def eval(self, testset_name):
         """Run eval and push to dat"""
         if getattr(self, f"{testset_name}_mean_df") is None:
             results = self.run_eval[testset_name]()
-     
-            if self.batch_mode:
-                results.to_sql(testset_name, self.con, if_exists="append")
         else:
             print("Evaluation results found, loaded from file.")
-                  
-        
 
     def _eval_train(self):
         testset_name = "train"
@@ -518,7 +509,7 @@ class EvalReading:
         )
 
         self.train_mean_df = mean_df
-        
+
         return df
 
     def _eval_strain(self):
@@ -532,7 +523,7 @@ class EvalReading:
             "strain_lf_con_hi",
             "strain_lf_inc_hi",
             "strain_lf_con_li",
-            "strain_lf_inc_li"
+            "strain_lf_inc_li",
         )
 
         for testset_name in testsets:
@@ -552,11 +543,8 @@ class EvalReading:
             t.eval_all()
             df = pd.concat([df, t.result])
 
-
         df.to_csv(
-            os.path.join(
-                self.cfg.path["model_folder"], "eval", "strain_item_df.csv"
-            )
+            os.path.join(self.cfg.path["model_folder"], "eval", "strain_item_df.csv")
         )
 
         # Condition level aggregate
@@ -575,12 +563,10 @@ class EvalReading:
             .reset_index()
         )
         mean_df.to_csv(
-            os.path.join(
-                self.cfg.path["model_folder"], "eval", "strain_mean_df.csv"
-            )
+            os.path.join(self.cfg.path["model_folder"], "eval", "strain_mean_df.csv")
         )
         self.strain_mean_df = mean_df
-        
+
         return df
 
     def _eval_grain(self):
@@ -640,7 +626,7 @@ class EvalReading:
         )
 
         self.grain_mean_df = mean_df
-        
+
         return df
 
     def _eval_taraban(self):
@@ -691,7 +677,7 @@ class EvalReading:
         )
 
         self.taraban_mean_df = mean_df
-        
+
         return df
 
     def _eval_cortese(self):
@@ -729,7 +715,7 @@ class EvalReading:
         )
 
         self.cortese_mean_df = mean_df
-        
+
         return df
 
     def plot_reading_acc(self, df):
@@ -760,3 +746,131 @@ class EvalReading:
         ]
         p = self.plot_reading_acc(df).encode(color="testset", strokeDash="y_test")
         return p
+
+
+# New set of plots
+
+
+def plot_reading_acc(df):
+    timetick_selection = alt.selection_single(
+        bind=alt.binding_range(min=0, max=12, step=1),
+        fields=["timetick"],
+        init={"timetick": 12},
+        name="timetick",
+    )
+
+    y_selection = alt.selection_single(
+        bind=alt.binding_select(options=["pho", "sem"]),
+        fields=["y"],
+    )
+
+    return (
+        alt.Chart(df)
+        .mark_line()
+        .encode(
+            x="epoch:Q",
+            y=alt.Y("mean(acc):Q", scale=alt.Scale(domain=(0, 1))),
+            color="testset",
+        )
+        .add_selection(timetick_selection)
+        .add_selection(y_selection)
+        .transform_filter(timetick_selection)
+        .transform_filter(y_selection)
+    )
+
+
+def plot_grain_acceptable(df):
+
+    df = df.loc[df.y_test.isin(["pho", "sem"]) & (df.y == "pho")]
+
+    timetick_selection = alt.selection_single(
+        bind=alt.binding_range(min=0, max=cfg.n_timesteps, step=1),
+        fields=["timetick"],
+        init={"timetick": cfg.n_timesteps},
+        name="timetick",
+    )
+
+    return (
+        alt.Chart(df)
+        .mark_line()
+        .encode(
+            x="epoch:Q",
+            y=alt.Y("acc:Q", scale=alt.Scale(domain=(0, 1))),
+            color="testset",
+        )
+        .add_selection(timetick_selection)
+        .transform_filter(timetick_selection)
+    )
+
+
+def plot_grain_by_resp(df):
+    df = df.loc[df.y_test.isin(["pho_large_grain", "pho_small_grain"])]
+    p = plot_reading_acc(df).encode(color="testset", strokeDash="y_test")
+    return p
+
+
+def plot_contrast(df, use_y="acc", y_max=1, task="triangle"):
+    df = df.loc[(df.task == task)]
+
+    timetick_selection = alt.selection_single(
+        bind=alt.binding_range(min=0, max=12, step=1),
+        fields=["timetick"],
+        init={"timetick": cfg.n_timesteps},
+        name="timetick",
+    )
+
+    y_selection = alt.selection_single(
+        bind=alt.binding_select(options=["pho", "sem"]),
+        init={"y": "pho"},
+        fields=["y"],
+    )
+
+    cond_selection = alt.selection_multi(bind="legend", fields=["testset"])
+
+    # Plot by condition
+    plot_by_cond = (
+        alt.Chart(df)
+        .mark_line()
+        .encode(
+            x="epoch:Q",
+            y=alt.Y(f"{use_y}:Q", scale=alt.Scale(domain=(0, y_max))),
+            color="testset:N",
+            opacity=alt.condition(cond_selection, alt.value(1), alt.value(0.1)),
+        )
+        .add_selection(timetick_selection)
+        .add_selection(y_selection)
+        .add_selection(cond_selection)
+        .transform_filter(timetick_selection)
+        .transform_filter(y_selection)
+    )
+
+    # Plot contrasts
+    contrasts = {}
+    contrasts[
+        "F contrast"
+    ] = """(datum.strain_hf_con_hi + datum.strain_hf_con_li + datum.strain_hf_inc_hi + datum.strain_hf_inc_li - 
+        (datum.strain_lf_con_hi + datum.strain_lf_con_li + datum.strain_lf_inc_hi + datum.strain_lf_inc_li))/4"""
+    contrasts[
+        "CON contrast"
+    ] = """(datum.strain_hf_con_hi + datum.strain_hf_con_li + datum.strain_lf_con_hi + datum.strain_lf_con_li - 
+        (datum.strain_hf_inc_hi + datum.strain_hf_inc_li + datum.strain_lf_inc_hi + datum.strain_lf_inc_li))/4"""
+    contrasts[
+        "IMG contrast"
+    ] = """(datum.strain_hf_con_hi + datum.strain_lf_con_hi + datum.strain_hf_inc_hi + datum.strain_lf_inc_hi - 
+        (datum.strain_hf_con_li + datum.strain_lf_con_li + datum.strain_hf_inc_li + datum.strain_lf_inc_li))/4"""
+
+    def create_contrast_plot(name):
+        return (
+            plot_by_cond.encode(
+                y=alt.Y("difference:Q", scale=alt.Scale(domain=(-y_max, y_max)))
+            )
+            .transform_pivot("testset", value=use_y, groupby=["epoch"])
+            .transform_calculate(difference=contrasts[name])
+            .properties(title=name, width=100, height=100)
+        )
+
+    contrast_plots = alt.hconcat()
+    for c in contrasts.keys():
+        contrast_plots |= create_contrast_plot(c)
+
+    return plot_by_cond | contrast_plots
