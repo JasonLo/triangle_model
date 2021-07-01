363490c29c8c9b9e2bb6867d546562f6daa4fa43
Jason Lo <lcmjlo@gmail.com>
2021-07-01 23:10:09 +0000

    minor change

--- src/metrics.py
+++ src/metrics.py
@@ -1,12 +1,92 @@
-# %%
-
-""" Custom metrics for diagnostic or experimental purpose"""
-
 import tensorflow as tf
 import tensorflow.keras.backend as K
+from tensorflow.python.keras.metrics import MeanMetricWrapper
 import pandas as pd
 
+# Loss
+class CustomBCE(tf.keras.losses.Loss):
+    """Binarycross entropy loss with variable zero-error-radius"""
 
+    def __init__(self, radius=0.1, name="bce_with_ZER"):
+        super().__init__(name=name)
+        self.radius = radius
+        self.sample_weights = None
+
+    def set_sample_weights(self, sample_weights):
+        """Method to set sample_weights for scaling losses
+        Cannot pass as an argument in call(), this is a work around
+        call will automatically scale losses if sample_weight exists
+        """
+        self.sample_weights = tf.convert_to_tensor(sample_weights, dtype=tf.float32)
+
+    def disable_losses_scaling(self):
+        self.sample_weights = None
+
+    @staticmethod
+    def zer_replace(target, output, zero_error_radius):
+        """Replace output by target if value within zero-error-radius"""
+        within_zer = tf.math.less_equal(
+            tf.math.abs(output - target), tf.constant(zero_error_radius)
+        )
+        return tf.where(within_zer, target, output)
+
+    @staticmethod
+    def scale_losses(losses, sample_weights):
+        """Multiply sample weights on losses"""
+        timetick_dim = losses.shape[0]
+        output_dim = losses.shape[2]
+
+        expanded_sample_weights = tf.tile(
+            sample_weights[tf.newaxis, :, tf.newaxis], [timetick_dim, 1, output_dim]
+        )
+
+        return losses * expanded_sample_weights
+
+    def call(self, y_true, y_pred):
+        y_pred = tf.convert_to_tensor(y_pred)
+        y_true = tf.cast(y_true, y_pred.dtype)
+
+        # Replace output by target if value within zero error radius
+        zer_output = self.zer_replace(y_true, y_pred, self.radius)
+
+        # Clip with a tiny constant to avoid zero division
+        epsilon_ = tf.convert_to_tensor(K.epsilon(), y_pred.dtype)
+        zer_output = tf.clip_by_value(zer_output, epsilon_, 1.0 - epsilon_)
+
+        # Compute cross entropy from probabilities.
+        bce = y_true * tf.math.log(zer_output + K.epsilon())
+        bce += (1 - y_true) * tf.math.log(1 - zer_output + K.epsilon())
+        bce *= -1
+
+        # Scale losses
+        if self.sample_weights is not None:
+            print(f"I have scaled sample weight by {self.sample_weights}")
+            bce = self.scale_losses(bce, self.sample_weights)
+
+        return bce
+
+
+# Stateless metrics (mean of all batch within an epoch) for tensorboard
+def sum_squared_error(y_true, y_pred):
+    return tf.reduce_sum(tf.square(y_pred - y_true), axis=-1)
+
+
+def right_side_accuracy(y_true, y_pred):
+    y_pred = tf.cast(y_pred > 0.5, y_pred.dtype)
+    return tf.reduce_min(tf.cast(tf.equal(y_true, y_pred), y_pred.dtype), axis=-1)
+
+
+class StatelessRightSideAccuracy(MeanMetricWrapper):
+    def __init__(self, name="stateless_right_side_acc", dtype=None):
+        super().__init__(right_side_accuracy, name, dtype=dtype)
+
+
+class StatelessSumSquaredError(MeanMetricWrapper):
+    def __init__(self, name="stateless_sse", dtype=None):
+        super().__init__(sum_squared_error, name, dtype=dtype)
+
+
+# Stateful metrics
 class PhoAccuracy(tf.keras.metrics.Metric):
     """Nearest phoneme based accuracy (HS04)"""
 
@@ -74,7 +154,6 @@ class PhoAccuracy(tf.keras.metrics.Metric):
         act_2d = tf.reshape(tf.cast(act, tf.float32), shape=(10, 25))
         return tf.vectorized_map(self.get_pho_idx_slot, act_2d)
 
-    @tf.function
     def get_pho_idx_batch(self, act):
         """Trio function for getting phoneme 3 (Batch):
         Get cloest distance pho idx in a batch
@@ -150,6 +229,7 @@ class SumSquaredError(tf.keras.metrics.Metric):
         self.out.assign(0.0)
 
 
+####  Experimental metrics ####
 class NodeCounter(tf.keras.metrics.Metric):
     """Export last slot average output in a batch"""
 
@@ -285,66 +365,3 @@ class InputOfOneTarget(tf.keras.metrics.Metric):
 
     def reset_state(self):
         self.out.assign(0.0)
-
-
-class CustomBCE(tf.keras.losses.Loss):
-    """Binarycross entropy loss with variable zero-error-radius"""
-
-    def __init__(self, radius=0.1, name="bce_with_ZER"):
-        super().__init__(name=name)
-        self.radius = radius
-        self.sample_weights = None
-
-    def set_sample_weights(self, sample_weights):
-        """Method to set sample_weights for scaling losses
-        Cannot pass as an argument in call(), this is a work around
-        call will automatically scale losses if sample_weight exists
-        """
-        self.sample_weights = tf.convert_to_tensor(sample_weights, dtype=tf.float32)
-
-    def disable_losses_scaling(self):
-        self.sample_weights = None
-
-    @staticmethod
-    def zer_replace(target, output, zero_error_radius):
-        """Replace output by target if value within zero-error-radius"""
-        within_zer = tf.math.less_equal(
-            tf.math.abs(output - target), tf.constant(zero_error_radius)
-        )
-        return tf.where(within_zer, target, output)
-
-    @staticmethod
-    def scale_losses(losses, sample_weights):
-        """Multiply sample weights on losses"""
-        timetick_dim = losses.shape[0]
-        output_dim = losses.shape[2]
-
-        expanded_sample_weights = tf.tile(
-            sample_weights[tf.newaxis, :, tf.newaxis], [timetick_dim, 1, output_dim]
-        )
-
-        return losses * expanded_sample_weights
-
-    def call(self, y_true, y_pred):
-        y_pred = tf.convert_to_tensor(y_pred)
-        y_true = tf.cast(y_true, y_pred.dtype)
-
-        # Replace output by target if value within zero error radius
-        zer_output = self.zer_replace(y_true, y_pred, self.radius)
-
-        # Clip with a tiny constant to avoid zero division
-        epsilon_ = tf.convert_to_tensor(K.epsilon(), y_pred.dtype)
-        zer_output = tf.clip_by_value(zer_output, epsilon_, 1.0 - epsilon_)
-
-        # Compute cross entropy from probabilities.
-        bce = y_true * tf.math.log(zer_output + K.epsilon())
-        bce += (1 - y_true) * tf.math.log(1 - zer_output + K.epsilon())
-        bce *= -1
-
-        # Scale losses
-        if self.sample_weights is not None:
-            print(f"I have scaled sample weight by {self.sample_weights}")
-            bce = self.scale_losses(bce, self.sample_weights)
-
-        return bce
-
