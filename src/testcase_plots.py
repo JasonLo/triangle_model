e5ae698df23c2645a8b9ec0d3ba6dda1656c0881
Jason Lo <lcmjlo@gmail.com>
2021-07-08 04:42:25 +0000

    change test case 3 to average over timetick

--- src/testcase_plots.py
+++ src/testcase_plots.py
@@ -17,6 +17,7 @@ def make_cond_mean_df(df):
     return df
 
 def plot_hs04_fig9(mean_df):
+    """test case 1"""
 
     timetick_selection = alt.selection_single(
         bind=alt.binding_range(min=0, max=12, step=1),
@@ -32,6 +33,8 @@ def plot_hs04_fig9(mean_df):
     ).add_selection(timetick_selection).transform_filter(timetick_selection)
 
 def plot_hs04_fig10(mean_df):
+    """test case 2"""
+
     epoch_selection = alt.selection_single(
         bind=alt.binding_range(min=0, max=291, step=10),
         fields=["epoch"],
@@ -47,20 +50,27 @@ def plot_hs04_fig10(mean_df):
     ).add_selection(epoch_selection).transform_filter(epoch_selection).properties(width=200, height=200)
 
 def plot_conds(mean_df):
-    timetick_selection = alt.selection_single(
-        bind=alt.binding_range(min=0, max=12, step=1),
-        fields=["timetick"],
-        init={"timetick": 12},
-        name="timetick",
-    )
+    """test case 3"""
 
-    return alt.Chart(mean_df).mark_line().encode(
+    # timetick_selection = alt.selection_single(
+    #     bind=alt.binding_range(min=0, max=12, step=1),
+    #     fields=["timetick"],
+    #     init={"timetick": 12},
+    #     name="timetick",
+    # )
+    sdf = mean_df.loc[(mean_df.timetick >= 4) & (mean_df.output_name=='pho')]
+
+    return alt.Chart(sdf).mark_line().encode(
         x='epoch:Q',
-        y=alt.Y('acc:Q', scale=alt.Scale(domain=(0,1))),
+        y=alt.Y('mean(acc):Q', scale=alt.Scale(domain=(0,1))),
         color='cond:N'
-    ).add_selection(timetick_selection).transform_filter(timetick_selection)
+    )
+    
+    # .add_selection(timetick_selection).transform_filter(timetick_selection)
 
 def plot_hs04_fig11(mean_df):
+    """test case 4"""
+
     epoch_selection = alt.selection_single(
         bind=alt.binding_range(min=0, max=291, step=10),
         fields=["epoch"],
