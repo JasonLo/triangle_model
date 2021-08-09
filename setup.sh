96209f1461d80821b1f82fae957caed8be6220e5
Jason Lo <lcmjlo@gmail.com>
2021-08-09 18:27:15 +0000

    move to docker

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-echo "/home/jupyter/tf/src" > /opt/conda/lib/python3.7/site-packages/tf_src.pth
-pip install -r /home/jupyter/tf/requirements.txt
+echo "/home/jupyter/triangle_model/src" > /opt/conda/lib/python3.7/site-packages/tf_src.pth
+pip install -r /home/jupyter/triangle_model/requirements.txt
 mkdir -p ~/.config/git
 nbstripout --install --global --attributes=~/.config/git/attributes
-cd /home/jupyter/tf
+cd /home/jupyter/triangle_model
 git config --global user.name "Jason Lo"
 git config --global user.email "lcmjlo@gmail.com"
 git config --global credential.helper store
