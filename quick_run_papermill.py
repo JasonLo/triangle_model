66cc92ebc971325f0cbf845ec92327fb979321bf
Jason Lo <lcmjlo@gmail.com>
2021-08-27 16:56:12 -0400

    script for running batch

---
+++ quick_run_papermill.py
@@ -0,0 +1,47 @@
+import os, argparse, papermill, json
+from tqdm import tqdm
+
+def run_batch(cfg, which_gpu:int):
+    """
+    Using papermill to run parameterized notebook
+    To prevent overwriting, set default overwrite to False if needed
+    """
+
+    print(f"Running model {cfg['code_name']} on GPU: {which_gpu}")
+    
+    # Inject GPU setting into cfg.params
+    cfg["params"]['which_gpu'] = which_gpu
+
+    os.makedirs(cfg["model_folder"], exist_ok=True)
+    papermill.execute_notebook(
+        cfg["in_notebook"],
+        cfg["out_notebook"],
+        parameters=cfg["params"],
+    )
+    clear_output()
+
+
+def main(batch_json, which_gpu:int=0):
+    """
+    Using papermill to run parameterized notebook
+    To prevent overwriting, set default overwrite to False if needed
+    """
+    with open(batch_json) as f:
+        batch_cfgs = json.load(f)
+
+
+    for cfg in tqdm(batch_cfgs):
+        run_batch(cfg, which_gpu)
+
+
+if __name__ == "__main__":
+    """Command line entry point, run batch with batch cfg"""
+    parser = argparse.ArgumentParser(description="Train TF model with config json")
+    parser.add_argument("-f", "--json_file", required=True, type=str)
+    parser.add_argument("-g", "--which_gpu", required=False, type=int)
+    args = parser.parse_args()
+
+    if args.which_gpu:
+        main(args.json_file, args.which_gpu)
+    else:
+        main(args.json_file)
