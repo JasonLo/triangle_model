8512699a6eee5b86568822a1d36f46ba48c04216
Jason Lo <lcmjlo@gmail.com>
2021-09-03 19:02:09 -0400

    clean code

--- run_papermill.py
+++ run_papermill.py
@@ -4,38 +4,38 @@ from tqdm import tqdm
 from time import sleep
 
 
-def run_one(cfg: dict, queue, which_gpu=None) -> int:
+def run_one(cfg: dict, free_gpu_queue, which_gpu=None):
     """Run one parameterized notebook.
 
     Keyword arguments:
     cfg -- a dictionary of config parameters to pass to the notebook
-    queue -- a multiprocessing.Queue() to record the status of the run
-
-    Returns:
-    which_gpu -- the GPU slot that was used (for the next queue to run on)
+    free_gpu_queue -- a multiprocessing.Queue() to record the status of the run
+    which_gpu -- the GPU to run the model on
     """
     # Inject GPU setting into cfg.params
-    # GPU allocation is done by individual notebook
+    # GPU instance is handle by each notebook
     cfg["params"]["which_gpu"] = which_gpu
 
-    logging.info(f"Start running model {cfg['code_name']} on {which_gpu}")
+    logging.info(f"Start running {cfg['code_name']} on {which_gpu}")
 
+    # Run the notebook with papermill
     papermill.execute_notebook(
         cfg["in_notebook"],
         cfg["out_notebook"],
         parameters=cfg["params"],
     )
 
-    logging.info(f"Finished running model {cfg['code_name']} on {which_gpu}")
+    logging.info(f"Finished running {cfg['code_name']} on {which_gpu}")
 
-    sleep(10)  # Allows GPU memory to release
-    queue.put(which_gpu)  # Record which GPU was used to queue
+    sleep(10)  # Allows GPU memory to release properly
+    free_gpu_queue.put(which_gpu)  # Release GPU
 
 
-def main(batch_json: str, resume_from: int=None):
+def main(batch_json: str, resume_from: int = None):
     """Run a batch of models."""
     # Set available GPUs for models to run on
     available_gpus = [0, 0, 1, 1, 2, 2]
+    
     # Load the batch json
     with open(batch_json) as f:
         batch_cfgs = json.load(f)
@@ -43,40 +43,42 @@ def main(batch_json: str, resume_from: int=None):
     if resume_from is not None:
         batch_cfgs = batch_cfgs[resume_from:]
 
-    # Create logging file and format
+    # Configure logging to track the progress of the run
     logging.basicConfig(
-        format='%(asctime)s %(levelname)-8s %(message)s',
+        format="%(asctime)s %(levelname)-8s %(message)s",
         level=logging.INFO,
-        datefmt='%Y-%m-%d %H:%M:%S',
-        filename=f"models/{batch_cfgs[0]['params']['batch_name']}/batch.log"
+        datefmt="%Y-%m-%d %H:%M:%S",
+        filename=f"models/{batch_cfgs[0]['params']['batch_name']}/run.log",
     )
 
-    # Create a queue to record which GPU was used
-    q = Queue()
-    processes = []
+    # Create a queue to record which GPU is not in use
+    free_gpu_queue = Queue()
 
     # Run the model
     for i, cfg in enumerate(batch_cfgs):
-        if i < 6:
-            # Cold start with default GPU allocations (which_gpu=None)
-            # processes.append(Process(target=run_one, args=(cfg, q, i%3)))
-            processes.append(Process(target=run_one, args=(cfg, q, available_gpus[i])))
-            processes[i].start()
-            sleep(1)
+        if i < len(available_gpus):
+            # Allocate one model to each available GPU
+            job = Process(target=run_one, args=(cfg, free_gpu_queue, available_gpus[i]))
+            job.start()
         else:
-            # Warm start with next available GPU
-            [p.join() for p in processes] # Detect finished process
-            released_gpu = q.get()
-            processes.append(Process(target=run_one, args=(cfg, q, released_gpu)))
-            processes[i].start()
-            sleep(1)
+            # Queue for next run: Wait for a GPU to be available and run the remaining models
+            while True:
+                try:
+                    free_gpu = free_gpu_queue.get()
+                    job = Process(target=run_one, args=(cfg, free_gpu_queue, free_gpu))
+                    job.start()
+                    break # Run suc
+                except:
+                    sleep(5) # Check every 5 seconds
+                    continue
 
 
 if __name__ == "__main__":
     """Command line entry point, run batch with batch cfg
     If run on server:
-        1) Put process to background by using "&"
+        1a) Put process to background by using "&"
         e.g.: python3 quick_run_papermill.py -f models/batch_run/batch_name/batch_config.json -g 1 &
+        1b) Alternatively, Ctrl+Z to pause the process, and bg to put it in background 
         2) Avoid job being kill after disconnection by calling "disown"
     """
     parser = argparse.ArgumentParser(description="Train TF model with config json")
