309a0ef38ac8300e2438bc3c85d5abdfd47ba916
Jason Lo <lcmjlo@gmail.com>
2021-09-04 04:43:05 +0000

    ignore output when diff ipynb

--- setup.sh
+++ setup.sh
@@ -18,5 +18,9 @@ git config --global user.email "lcmjlo@gmail.com"
 git config --global credential.helper store
 git fetch
 mkdir -p ~/.config/git
-nbstripout --install --global --attributes=~/.config/git/attributes
+# ignore output when diff-ing ipynb 
+echo "*.ipynb diff=jupyternotebook" > /home/jupyter/triangle_model/.gitattributes
+echo "[filter \"jupyternotebook\"]" >> /home/jupyter/triangle_model/.git/config
+echo -e "\tclean = jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace %f" >> /home/jupyter/triangle_model/.git/config
+echo -e "\trequired" >> /home/jupyter/triangle_model/.git/config
 
