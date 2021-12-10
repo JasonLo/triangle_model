3c881a406adf7c207e84183430c0589c1a21540f
Jason Lo <lcmjlo@gmail.com>
2021-12-10 12:20:11 -0500

    create new trimmed pho train_r100_trimmed  testset

--- src/data_wrangling.py
+++ src/data_wrangling.py
@@ -11,31 +11,31 @@ from typing import List
 load_dotenv()
 tf_root = os.environ.get("TF_ROOT")
 
-# Preprocessing
+# Preprocessing or convienience functions for looking at raw data
 
 
 def get_duplicates(df: pd.DataFrame) -> dict:
-    """Analyze duplicates word entries in a dataframe."""
+    """Analyze duplicates word and thier counts in a dataframe."""
     duplicates = df.groupby(["word", "ort", "pho"]).size().reset_index(name="counts")
     duplicates = duplicates[duplicates.counts > 1]
     return dict(zip(duplicates.word, duplicates.counts))
 
 
-def get_index_of_one(l: list) -> list:
+def get_index_of_one(rep: list) -> list:
     """Return the index of 1 values in a list.
     For compressing a dense representation into a sparse one.
 
     Arguments:
-    l: List of values. (Assume 1d list)
+        representation: List of values. (Assume 1d list)
     """
-    return [i for i, x in enumerate(l) if x == 1]
+    return [i for i, x in enumerate(rep) if x == 1]
 
 
 def get_used_slots(rep: list) -> set:
     """Get the index of non empty slots in a list of representation.
 
     Arguments:
-        rep: List of representation.
+        rep: List of representation
     """
     return {i for i, x in enumerate(rep) if x != "_"}
 
@@ -44,8 +44,8 @@ def remove_slots(rep: list, remove_slots: list) -> list:
     """Removing a slot from a list of representation.
 
     Arguments:
-        rep: List of representation.
-        slot: Slot to remove.
+        rep: List of representation
+        slot: Slot to remove
     """
     l = [len(x) for x in rep]
     assert len(set(l)) == 1  # Check that all the representation have the same length
@@ -58,7 +58,7 @@ def trim_unused_slots(rep: list) -> list:
     """Trimming unused slot in a list of representation.
 
     Arguments:
-        rep: List of representation.
+        rep: List of representation
     """
     slots = range(len(rep[0]))
     print(f"We have these slots: {list(slots)}")
@@ -127,13 +127,16 @@ def pho_to_binary(pho: List[str]) -> np.array:
 
 
 def get_pronunciation_fast(act: np.array, phon_key: dict = None) -> str:
+    """Get a pronunciation from activations. Optimized for compute speed."""
     if phon_key is None:
         phon_key = gen_pkey()
     phonemes = list(phon_key.keys())
-    act10 = np.tile([v for k, v in phon_key.items()], 10)
+
+    n_slots = len(act) // 25
+    act10 = np.tile([v for v in phon_key.values()], n_slots)
 
     d = np.abs(act10 - act)
-    d_mat = np.reshape(d, (38, 10, 25))
+    d_mat = np.reshape(d, (38, n_slots, 25))
     sumd_mat = np.squeeze(np.sum(d_mat, 2))
     map_idx = np.argmin(sumd_mat, 0)
     out = str()
@@ -144,108 +147,110 @@ def get_pronunciation_fast(act: np.array, phon_key: dict = None) -> str:
 
 def get_batch_pronunciations_fast(act: np.array, phon_key: dict = None) -> np.array:
     """Get a batch of pronunciations from activations."""
+
     if phon_key is None:
         phon_key = gen_pkey()
     return np.apply_along_axis(get_pronunciation_fast, 1, act, phon_key)
 
 
-class MyData:
-    """
-    This object load all clean data from disk (both training set and testing sets)
-    Also calculate sampling_p according to cfg.sample_name setting
-    """
+# class MyData:  # Obsolete
+#     """
+#     This object load all clean data from disk (both training set and testing sets)
+#     Also calculate sampling_p according to cfg.sample_name setting
+#     """
 
-    def __init__(self, input_path="dataset"):
+#     def __init__(self, input_path="dataset"):
 
-        self.input_path = os.path.join(tf_root, input_path)
+#         self.input_path = os.path.join(tf_root, input_path)
 
-        # init an empty testset dict for new testset format
-        # first level: testset name
-        # second level (in each testset): word, ort, pho, sem
-        self.testsets = {}
-        self.load_all_testsets()
+#         # init an empty testset dict for new testset format
+#         # first level: testset name
+#         # second level (in each testset): word, ort, pho, sem
+#         self.testsets = {}
+#         self.load_all_testsets()
 
-        self.np_representations = {
-            "ort": np.load(os.path.join(self.input_path, "ort_train.npz"))["data"],
-            "pho": np.load(os.path.join(self.input_path, "pho_train.npz"))["data"],
-            "sem": np.load(os.path.join(self.input_path, "sem_train.npz"))["data"],
-        }
+#         self.np_representations = {
+#             "ort": np.load(os.path.join(self.input_path, "ort_train.npz"))["data"],
+#             "pho": np.load(os.path.join(self.input_path, "pho_train.npz"))["data"],
+#             "sem": np.load(os.path.join(self.input_path, "sem_train.npz"))["data"],
+#         }
 
-        self.df_train = pd.read_csv(
-            os.path.join(self.input_path, "df_train.csv"), index_col=0
-        )
+#         self.df_train = pd.read_csv(
+#             os.path.join(self.input_path, "df_train.csv"), index_col=0
+#         )
 
-        self.df_strain = pd.read_csv(
-            os.path.join(self.input_path, "df_strain.csv"), index_col=0
-        )
+#         self.df_strain = pd.read_csv(
+#             os.path.join(self.input_path, "df_strain.csv"), index_col=0
+#         )
 
-        self.df_grain = pd.read_csv(
-            os.path.join(self.input_path, "df_grain.csv"), index_col=0
-        )
+#         self.df_grain = pd.read_csv(
+#             os.path.join(self.input_path, "df_grain.csv"), index_col=0
+#         )
 
-        self.df_taraban = pd.read_csv(
-            os.path.join(self.input_path, "df_taraban.csv"), index_col=0
-        )
+#         self.df_taraban = pd.read_csv(
+#             os.path.join(self.input_path, "df_taraban.csv"), index_col=0
+#         )
 
-        self.df_glushko = pd.read_csv(
-            os.path.join(self.input_path, "df_glushko.csv"), index_col=0
-        )
-        self.x_glushko = np.load(os.path.join(self.input_path, "ort_glushko.npz"))[
-            "data"
-        ]
-        self.x_glushko_wf = np.array(self.df_glushko["wf"])
-        self.x_glushko_img = np.array(self.df_glushko["img"])
+#         self.df_glushko = pd.read_csv(
+#             os.path.join(self.input_path, "df_glushko.csv"), index_col=0
+#         )
+#         self.x_glushko = np.load(os.path.join(self.input_path, "ort_glushko.npz"))[
+#             "data"
+#         ]
+#         self.x_glushko_wf = np.array(self.df_glushko["wf"])
+#         self.x_glushko_img = np.array(self.df_glushko["img"])
 
-        with open(os.path.join(self.input_path, "y_glushko.pkl"), "rb") as f:
-            self.y_glushko = pickle.load(f)
+#         with open(os.path.join(self.input_path, "y_glushko.pkl"), "rb") as f:
+#             self.y_glushko = pickle.load(f)
 
-        with open(os.path.join(self.input_path, "pho_glushko.pkl"), "rb") as f:
-            self.pho_glushko = pickle.load(f)
+#         with open(os.path.join(self.input_path, "pho_glushko.pkl"), "rb") as f:
+#             self.pho_glushko = pickle.load(f)
 
-        self.phon_key = gen_pkey()
+#         self.phon_key = gen_pkey()
 
-    def word_to_idx(self, word, cond=None, skip_duplicates=True):
-        # TODO: Handle duplicate later
+#     def word_to_idx(self, word, cond=None, skip_duplicates=True):
+#         # TODO: Handle duplicate later
 
-        idx = self.df_train.word.loc[self.df_train.word == word].index
-        if (len(idx) == 1) or (not skip_duplicates):
-            return idx.to_list()[0], cond
+#         idx = self.df_train.word.loc[self.df_train.word == word].index
+#         if (len(idx) == 1) or (not skip_duplicates):
+#             return idx.to_list()[0], cond
 
-    def words_to_idx(self, words, conds):
-        xs = list(map(self.word_to_idx, words, conds))
-        idx = [x[0] for x in xs if x is not None]
-        cond = [x[1] for x in xs if x is not None]
-        return idx, cond
+#     def words_to_idx(self, words, conds):
+#         xs = list(map(self.word_to_idx, words, conds))
+#         idx = [x[0] for x in xs if x is not None]
+#         cond = [x[1] for x in xs if x is not None]
+#         return idx, cond
 
-    def create_testset_from_words(self, words, conds):
-        """Create a testset dictionary package with a list of words"""
-        idx, cond = self.words_to_idx(words, conds)
-        return self.create_testset_from_train_idx(idx, cond)
+#     def create_testset_from_words(self, words, conds):
+#         """Create a testset dictionary package with a list of words"""
+#         idx, cond = self.words_to_idx(words, conds)
+#         return self.create_testset_from_train_idx(idx, cond)
 
-    def create_testset_from_train_idx(self, idx, cond=None):
-        """Return a test set representation dictionary with word, ort, pho, sem"""
-        return {
-            "item": list(self.df_train.loc[idx, "word"].astype("str")),
-            "cond": cond,
-            "ort": tf.constant(self.np_representations["ort"][idx], dtype=tf.float32),
-            "pho": tf.constant(self.np_representations["pho"][idx], dtype=tf.float32),
-            "sem": tf.constant(self.np_representations["sem"][idx], dtype=tf.float32),
-            "phoneme": get_batch_pronunciations_fast(
-                self.np_representations["pho"][idx]
-            ),
-        }
+#     def create_testset_from_train_idx(self, idx, cond=None):
+#         """Return a test set representation dictionary with word, ort, pho, sem"""
+#         return {
+#             "item": list(self.df_train.loc[idx, "word"].astype("str")),
+#             "cond": cond,
+#             "ort": tf.constant(self.np_representations["ort"][idx], dtype=tf.float32),
+#             "pho": tf.constant(self.np_representations["pho"][idx], dtype=tf.float32),
+#             "sem": tf.constant(self.np_representations["sem"][idx], dtype=tf.float32),
+#             "phoneme": get_batch_pronunciations_fast(
+#                 self.np_representations["pho"][idx]
+#             ),
+#         }
 
-    def load_all_testsets(self):
+#     def load_all_testsets(self):
 
-        all_testsets = ("strain", "grain")
+#         all_testsets = ("strain", "grain")
 
-        for testset in all_testsets:
-            file = os.path.join(self.input_path, "testsets", testset + ".pkl.gz")
-            self.testsets[testset] = load_testset(file)
+#         for testset in all_testsets:
+#             file = os.path.join(self.input_path, "testsets", testset + ".pkl.gz")
+#             self.testsets[testset] = load_testset(file)
 
 
 def load_testset(file):
-    """Load testset from pickled file"""
+    """Load testset from pickled file."""
+
     tf_root = os.environ.get("TF_ROOT")
     try:
         with gzip.open(file, "rb") as f:
@@ -260,3 +265,21 @@ def load_testset(file):
 def save_testset(testset, file):
     with gzip.open(file, "wb") as f:
         pickle.dump(testset, f)
+
+
+def create_testset_from_words(words, conds=None) -> dict:
+    """Create a testset dictionary package with a list of words."""
+
+    train = load_testset("dataset/train.pkl.gz")
+    sel_idx = [train["item"].index(word) for word in words if word in train["item"]]
+    print(f"{len(sel_idx)} out of {len(words)} words in new train set")
+
+    return {
+        "item": [train["item"][idx] for idx in sel_idx],
+        "phoneme": [train["phoneme"][idx] for idx in sel_idx],
+        "graphem": [train["graphem"][idx] for idx in sel_idx],
+        "ort": tf.gather(train["ort"], sel_idx),
+        "pho": tf.gather(train["pho"], sel_idx),
+        "sem": tf.gather(train["sem"], sel_idx),
+        "cond": conds,
+    }
